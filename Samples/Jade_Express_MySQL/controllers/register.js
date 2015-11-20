dba79645689edcb5f182d4261a02995a54b121b3
Robin Sillem <rsillem@scottlogic.com>
2015-11-20 14:33:29 +0000

    Makes registration workflow consistent

--- Samples/Jade_Express_MySQL/controllers/register.js
+++ Samples/Jade_Express_MySQL/controllers/register.js
@@ -17,7 +17,7 @@ router.post('/', function (req, res, next) {
         if (err) {
             return next(err);
         } else {
-            res.redirect('/');
+            res.redirect('/login');
         }
     });
 });
