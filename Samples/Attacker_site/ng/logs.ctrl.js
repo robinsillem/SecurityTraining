a39875524649a391b96dbb5393bc28fed592f414
dpizzi-scottlogic <dpizzi@scottlogic.com>
2015-11-30 10:09:06 +0000

    Add attacker site  which is in charge of collecting any stolen information such as authenticated session token

---
+++ Samples/Attacker_site/ng/logs.ctrl.js
@@ -0,0 +1,9 @@
+﻿angular.module('app')
+.controller('LogsCtrl', function ($scope, LogsSvc) {
+    LogsSvc.fetch()
+    .success(function (logs) {
+        $scope.logs = logs;
+    });
+
+});
+
---
+++ Samples/Attacker_site/server.js
@@ -0,0 +1,33 @@
+﻿var express = require('express');
+var bodyParser = require('body-parser');
+var config = require('./config');
+var winston = require('winston');
+var expressWinston = require('express-winston');
+
+var app = express();
+app.use(bodyParser.json());
+
+app.use(expressWinston.logger({
+    transports: [
+        new winston.transports.File({
+            json: true,
+            filename: './logs/request.log'
+        })
+    ]
+}));
+
+app.use('/api/logs', require('./controllers/api/logs'));
+app.use(require('./controllers/static'));
+
+app.use(expressWinston.errorLogger({
+    transports: [
+        new winston.transports.File({
+            json: true,
+            filename: './logs/error.log'
+        })
+    ]
+}));
+
+app.listen(config.port, function () {
+    console.log('Server listening on', config.port);
+});
