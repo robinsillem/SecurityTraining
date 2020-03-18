01a9e3a865c41712ddd8c87013754cebb74df299
Robin Sillem <robin.sillem@digital.justiice.gov.uk>
2020-03-18 12:05:17 +0000

    Version updates

--- Samples/Jade_Express_MySQL/vagrant_bootstrap.sh
+++ Samples/Jade_Express_MySQL/vagrant_bootstrap.sh
@@ -13,12 +13,9 @@ mysql -u root -psec_training sec_training < /vagrant/db/create.sql
 mysql -u root -psec_training some_other_database < /vagrant/db/create.sql
 mysql -u root -psec_training -e "GRANT ALL PRIVILEGES ON sec_training.* To 'sec_train_web'@'localhost' IDENTIFIED BY 'web_pass'"
 
-# Hack to get around Windows vs ubuntu permissioning issues on vagrant shared folders - npm install somewhere else and symlink
-cd /tmp
-cp /vagrant/package.json .
+rm -rf /vagrant/node_modules
+cd /vagrant
 sudo npm install
-rm /vagrant/node_modules
-ln -s /tmp/node_modules /vagrant/node_modules
 
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 sudo npm install -g gulp
@@ -26,5 +23,4 @@ sudo npm install -g gulp
 cp /vagrant/Jade_Express_MySQL.conf /etc/init
 
 # And start the server
-cd /vagrant
 gulp dev
--- Samples/MEAN_stack/vagrant_bootstrap.sh
+++ Samples/MEAN_stack/vagrant_bootstrap.sh
@@ -1,28 +1,25 @@
 #!/bin/sh
 
 # Required for mongodb install
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+echo "deb https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 
 sudo apt-get update
 sudo apt-get install -y nodejs
 sudo apt-get install -y npm
 sudo apt-get install -y mongodb-org
 
-# Hack to get around Windows vs ubuntu permissioning issues on vagrant shared folders - npm install somewhere else and symlink
-mkdir /tmp
-cd /tmp
-cp /vagrant/package.json .
+sudo service mongod start
+
+rm -rf /vagrant/node_modules
+cd /vagrant
 sudo npm install
-rm /vagrant/node_modules
-ln -s /tmp/node_modules /vagrant/node_modules
 
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 sudo npm install -g gulp
 
 cp /vagrant/MEAN_stack.conf /etc/init
-
-cd /vagrant
 
 # And start the server
 gulp dev
